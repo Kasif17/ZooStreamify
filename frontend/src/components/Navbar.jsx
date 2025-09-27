67fa83f834ce169714ff5279e992ab8793c47cc0
Kasif17 <developer786kasif@gmail.com>
2025-09-27 17:09:02 +0530

    Chat page and chatAPI fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,12 +28,14 @@ const Navbar = () => {
             <div className='pl-5'>
                <Link to='/' className='flex items-center gap-2.5'>
                <ShipWheelIcon className='size-9 text-primary'/>
-               <span className='text-3xl font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider'></span>
+               <span className='text-3xl font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider'>
+                ZooStreamy
+               </span>
                </Link>
             </div>
            )}
           
-          <div className='flex items-center gap-3 sm:gap-4'>
+          <div className='flex items-center gap-3 sm:gap-4 ml-auto'>
            <Link to={"/notification"}>
            <button className='btn btn-ghost btn-circle'>
               <BellIcon className='h-6 text-base-content opacity-70'/>
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import useAuthUser from '../hooks/useAuthUser';
+import { useQuery } from '@tanstack/react-query';
+import { getStreamToken } from '../lib/api';
+import {Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+import toast from 'react-hot-toast';
+import ChatLoader from '../components/ChatLoader';
+import CallButton from '../components/CallButton';
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const ChatPage = () => {
-  return (
-    <div>
-      ChatPage
+  const {id:targetUserId} = useParams();
+  
+  const [chatClient, setChatClient] = useState(null);
+  const [channel , setChannel] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const {authUser} = useAuthUser();
+
+  const {data:TokenData} = useQuery({
+    queryKey : ["streamToken"],
+    queryFn: getStreamToken,
+    enabled : !!authUser 
+  })
+  
+  useEffect(()=>{
+    const initChat = async ()=>{
+    if(!TokenData?.token || !authUser) return;
+    try {
+      console.log("Initializing stream chat client...");
+      const client = StreamChat.getInstance(STREAM_API_KEY)
+      await client.connectUser({
+        id: authUser._id,
+        name: authUser.fullName,
+        image: authUser.profilePic,
+      },TokenData.token)
+
+      const channelId = [authUser._id , targetUserId].sort().join("-");
+
+      const currChannel = client.channel("messaging",channelId,{
+        members:[authUser._id, targetUserId],
+      })
+      await currChannel.watch();
+
+      setChatClient(client);
+      setChannel(currChannel);
+      
+
+    } catch (error) {
+      console.log("Error initializing chat:", error);
+      toast.error("Could not connect to chat. Please try again")
+    } finally{
+      setLoading(false)
+    }
+    }
+    initChat()
+  },[TokenData, authUser, targetUserId])
+
+  const handleVideoCall = ()=>{
+
+  }
+
+  if(loading || !chatClient || !channel) return <ChatLoader/>
+
+  
+  return <div className='h-[93vh] w-full'>
+       <Chat client={chatClient}>
+          <Channel channel={channel}>
+             <div className='w-full relative'>
+              <CallButton handleVideoCall = {handleVideoCall}/>
+               <Window>
+                <ChannelHeader/>
+                <MessageList/>
+                <MessageInput focus />
+               </Window>
+             </div>
+             <Thread/>
+          </Channel>
+       </Chat>
     </div>
-  )
+  
 }
 
 export default ChatPage
