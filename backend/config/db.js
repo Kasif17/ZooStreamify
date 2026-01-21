696ac751dccf99d8b4bc4c29f5da6fef7764d168
Kasif17 <developer786kasif@gmail.com>
2026-01-21 11:40:13 +0530

    Add README and project screenshot

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const mongoDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://developer786kasif_db_user:9956920829@cluster0.sd78rha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" );
+        await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://developer786kasif_db_user:lQ7OqAmGTbvvs1GF@cluster0.sd78rha.mongodb.net/test" );
         console.log("Connected successfully ");
     } catch (error) {
         console.log("DB not connected",error);
